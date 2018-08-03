c70de92819dee24b039ad25be63f35d954c7b059
Jason <Jason@Colins-MacBook-Pro.local>
2018-08-03 01:32:14 -1000

    added footer, comments, and author

--- app.js
+++ app.js
@@ -5,31 +5,78 @@ const request = (url, callback) => {
   oReq.send();
 };
 
-const header = document.createElement('header');
+const header = document.createElement('div');
+header.id = 'headerId';
+header.src = './assets/header_bg.svg';
 bigBoxId.appendChild(header);
 
-const div1 = document.createElement('div');
-div1.id = 'div1Id';
-bigBoxId.appendChild(div1);
+const picBox = document.createElement('div');
+picBox.id = 'picBoxId'
+bigBoxId.appendChild(picBox);
 
-const div2 = document.createElement('div');
-div2.id = 'div2Id';
-bigBoxId.appendChild(div2);
+const logoDiv = document.createElement('img');
+logoDiv.src = './assets/logo.svg';
+logoDiv.id = 'logoDivId';
+header.appendChild(logoDiv);
+
+const navUlDiv = document.createElement('ul');
+navUlDiv.id = 'ulDivId';
+header.appendChild(navUlDiv);
+
+const navLiDiv1 = document.createElement('li');
+navLiDiv1.className = 'navLiId';
+navUlDiv.appendChild(navLiDiv1);
+const navLiDiv2 = document.createElement('li');
+navLiDiv2.className = 'navLiId';
+navUlDiv.appendChild(navLiDiv2);
+const navLiDiv3 = document.createElement('li');
+navLiDiv3.className = 'navLiId';
+navUlDiv.appendChild(navLiDiv3);
+
+navLiDiv1.innerHTML = 'Random';
+navLiDiv2.innerHTML = 'Get Boards';
+navLiDiv3.innerHTML = 'Get The App'
 
 
-
-request('https://www.reddit.com/r/oddlysatisfying.json', res => {
+request('https://www.reddit.com/r/gaming.json', res => {
   const data = JSON.parse(res.currentTarget.response);
-  div1.innerHTML = data.data.children[24].data.title
-  div2.innerHTML = data.data.children[23].data.title;
-  let image = document.createElement('img');
-  image.src = 'https://b.thumbs.redditmedia.com/PCMi7LsKrYKezgzltt_8mR7ELAPSqTj7QJ0ftPUbcsQ.jpg'
-  div1.appendChild(image);
-  console.log('response object', data.data.children)
+  for(let i = 0; i < 9; i++) {
+    let createCardDiv = document.createElement('div');
+    createCardDiv.className = 'bigDivClass';
+    picBox.appendChild(createCardDiv);
+    
+    let createTitleDiv = document.createElement('div');
+    createTitleDiv.className = 'titleDivClass';
+    createTitleDiv.innerHTML = data.data.children[i].data.title;
+    createCardDiv.appendChild(createTitleDiv);
+    
+    
+    let image = document.createElement('img');
+    image.className = 'imageClass';
+    image.src = data.data.children[i].data.url
+    createCardDiv.appendChild(image);
+    
+    let createCommentDiv = document.createElement('div');
+    createCommentDiv.innerHTML = 'Comments: ' + data.data.children[i].data.num_comments;
+    createCardDiv.appendChild(createCommentDiv);
 
+    let createAuthorDiv = document.createElement('div')
+    createAuthorDiv.innerHTML = 'Author: ' + data.data.children[i].data.author;
+    createCardDiv.appendChild(createAuthorDiv);
+    console.log(data.data.children[i].data)
+  }
 })
 
+const createFooter = document.createElement('div');
+createFooter.id = 'footerId';
+bigBoxId.appendChild(createFooter);
 
+const instagramImg = document.createElement('img');
+instagramImg.src = './assets/instagram_grey.svg';
+createFooter.appendChild(instagramImg);
+const facebookImg = document.createElement('img');
+facebookImg.src = './assets/facebook_grey.svg';
+createFooter.appendChild(facebookImg);
 
 // request(url, res => {
 //   const data = JSON.parse(res.currentTarget.response);
